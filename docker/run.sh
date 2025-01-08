ad9b65c6d642cdb8b4a00091d3a2902e9a131b57
DongjaeLEE <dongjae0107@gmail.com>
2025-01-08 13:06:19 +0900

    modify docker run command

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,5 @@ xhost +
 docker run --rm -it --ipc=host --net=host --privileged \
     --env="DISPLAY" \
     --volume="/etc/localtime:/etc/localtime:ro" \
-    --volume="/media/ori/Extreme SSD/oxford_spires/sequences:/dataset" \
-    --volume="/home/ori/Downloads/sc_lio_sam:/sc_lio_sam" \
     dongjae0107/lt-mapper:latest
 xhost -
